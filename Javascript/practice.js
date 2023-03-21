c34bd9d965c8b165688e1388d40900bd4bbacebc
simran0963 <simranmehta0963@gmail.com>
2023-03-22 02:56:00 +0530

    intermediate js complete

---
+++ Javascript/practice.js
@@ -0,0 +1,42 @@
+/*var arr = [];
+var i=1;
+
+// using looping: 
+function fizzBuzz(){
+    while(i<=100){
+        if(i%15==0){
+            arr.push("FizzBuzz");
+        }
+        else if(i%3==0){
+            arr.push("Fizz");
+        }
+        else if(i%5==0){
+            arr.push("Buzz");
+        }
+        else{
+            arr.push(i);
+        }
+        i++;
+    }
+    console.log(arr);
+}
+
+// Function call everytime:
+
+function fizzBuzz(){
+    if(i%15===0){
+        arr.push("fizzBuzz");
+    }
+   else if(i%3===0){
+       arr.push("Fizz");
+   }
+   else if(i%5===0){
+        arr.push("Buzz");
+    }
+    else{
+        arr.push(i);
+    }
+    i++;
+    console.log(arr)
+}
+*/
